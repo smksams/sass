42544c4d05dc76a17582b0ad7b42dd0cd667b634
Samuel dos Santos Garzotto Dalla Valle Filho <fsantos.samuca@gmail.com>
2024-04-18 14:26:12 -0300

    export default

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const LineChart = () => {
+const LineChart = () => {
   return (
     <div>LineChart</div>
   )
 }
+
+export default LineChart
--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const Customer = () => {
+const Customer = () => {
   return (
     <div>Customer</div>
   )
 }
+
+export default Customer
