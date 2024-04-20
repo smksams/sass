337441bde37e19acd69259f6dc6a81c7225e36d6
Samuel dos Santos Garzotto Dalla Valle Filho <fsantos.samuca@gmail.com>
2024-04-20 14:07:54 -0300

    navbar && sidebar updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,14 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import { links } from '../data/dummy'
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext()
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+
+  const handleCloseSideBar = () => {
+    if(activeMenu && screenSize < 900) {
+      setActiveMenu(false)
+    }
+  }
+
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray'
 
@@ -22,7 +29,7 @@ const Sidebar = () => {
               <SiShopware /> <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position='BottomCenter'>
-              <button type='button' onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
+              <button type='button' onClick={handleCloseSideBar} className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
@@ -34,7 +41,7 @@ const Sidebar = () => {
                   {item.title}
                 </p>
                 {item.links.map((link) => (
-                  <NavLink to={`/${link.name}`} key={link.name} onClick={() => { }} className={({ isActive }) => isActive ? activeLink : normalLink}>
+                  <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSideBar} className={({ isActive }) => isActive ? activeLink : normalLink}>
                     {link.icon}
                     <span className='capitalize'>{link.name}</span>
                   </NavLink>
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from "react";
 const StateContext = createContext()
 
 const initialState = {
-  chart: false,
+  chat: false,
   cart: false,
   userProfile: false,
   notification: false,
@@ -11,11 +11,21 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true)
+  const [isClicked, setIsClicked] = useState(initialState)
+  const [screenSize, setScreenSize] = useState(undefined)
 
+  const handleClick = (clicked) => {
+    setIsClicked({...initialState, [clicked] : true})
+  }
   return (
     <StateContext.Provider value={{
       activeMenu,
-      setActiveMenu
+      setActiveMenu,
+      isClicked, 
+      setIsClicked,
+      handleClick,
+      screenSize, 
+      setScreenSize,
     }}>
       {children}
     </StateContext.Provider>
